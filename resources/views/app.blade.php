fe5cb9da43d6f420fabf81912b11f999bf6bb9bb
Rifqi <f8goodspoof@gmail.com>
2026-06-19 22:44:27 +0700

    First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    <title>Rifqi | Pelayanan Jasa Ac</title>
+  </head>
+  <body>
+    <div>
+        <h1 class="text-3xl">
+          Rifqi
+        </h1>
+    </div>
+  </body>
+</html>

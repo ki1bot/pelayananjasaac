54200f21e95ee13d01de795ef3268276f341c13d
ki1bot Rifqi <f8goodspoof@gmail.com>
2026-09-15 19:45:15 +0700

    fix inertia vue typescript resolver

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,13 +1,13 @@
 import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { createApp, h } from "vue";
+import { createApp, h, type DefineComponent } from "vue";
 
 createInertiaApp({
     title: (title) => (title ? `${title} - Laravel` : "Laravel"),
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob("./Pages/**/*.vue"),
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue"),
         ),
     setup({ el, App, props, plugin }) {
         createApp({

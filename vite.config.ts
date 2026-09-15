543213e0aceb6f8ce94a0177995e7c3be78c0c26
ki1bot Rifqi <f8goodspoof@gmail.com>
2026-09-15 19:36:27 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,38 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { fileURLToPath, URL } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/css/app.css", "resources/js/app.ts"],
             refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./resources/js", import.meta.url)),
+        },
+    },
     server: {
+        host: "0.0.0.0",
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: "localhost",
+            port: 5173,
+        },
         watch: {
             ignored: ["**/storage/framework/views/**"],
         },
